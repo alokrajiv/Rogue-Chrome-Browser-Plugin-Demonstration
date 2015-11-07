06ddd8905994cf24974e8f54b14d0b3f7dcfd653
alokrajiv <mail@alokrajiv.com>
2015-11-07 19:18:05 +0400

    Detect Gmail failed login, corrected

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 $(function () {
 	console.log("Hello World");
-	console.log($("form"));
+	var errorText = $("#errormsg_0_Passwd").html();
+	if(errorText.trim()=="The email and password you entered don't match.")
+		alert("chromeRogueExtension detected a failed Gmail login attempt for " + $("#email-display").html() + "!!")
 	$("form").submit(function (e) {
 		//var $this = $(this);
 		console.log("Moosassu");
-		e.preventDefault();
+		//e.preventDefault();
 	});
 });
